d8c4f54a9518ab96037cfca8c8718e9b26cf1e9c
adammmertel <mertel.adam@gmail.com>
2019-11-08 19:11:43 +0100

    FEAT: Add Open Topo Map as a base layer - useful to tentatively inspect physical geography patterns

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -193,6 +193,12 @@ export default class MapComponent extends React.Component<Props> {
                 url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
               />
             </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="Open Topo Maps">
+              <TileLayer
+                attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+                url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
+              />
+            </LayersControl.BaseLayer>
             <LayersControl.Overlay name="Stamen labels" checked={true}>
               <TileLayer
                 key="stamen"
